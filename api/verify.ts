c3f098ecd99eba83e6e4c73944e11973638fc07b
tate <tateflyer1@gmail.com>
2020-12-02 12:07:11 -0500

    feat: verify endpoint

---
+++ api/verify.ts
@@ -0,0 +1,65 @@
+import { NowRequest, NowResponse } from '@vercel/node';
+import Arweave from "arweave";
+import { JWKInterface } from 'arweave/node/lib/wallet';
+import { google } from "googleapis";
+import { isVerified, tipReceived } from "arverify";
+
+export default function (req: NowRequest, res: NowResponse) {
+  const address: string | string[] = req.query.address;
+  const returnURI: string | string[] = req.query.return;
+  const jwk: JWKInterface = JSON.parse(process.env.jwk);
+
+  const client: Arweave = new Arweave({
+    host: "arweave.net",
+    port: 443,
+    protocol: "https"
+  });
+
+  const oauthClient = new google.auth.OAuth2(
+    process.env.gClientID,
+    process.env.gClientSecret,
+    process.env.endpoint + (process.env.endpoint.endsWith("/") ? "" : "/") + "verify/callback"
+  );
+
+  // Check if address was passed in
+  if (!address) {
+    res.statusCode = 400;
+    res.send({
+      status: "error",
+      message: "address is reqired"
+    });
+  }
+
+  // Check if the address is verified
+  // @ts-expect-error
+  if ((await isVerified(address)).verified) {
+    res.send({
+      status: "success",
+      message: "already verified",
+    });
+  }
+
+  // Check if a tip has been received
+  // @ts-expect-error
+  if (!(await tipReceived(address), await client.wallets.jwkToAddress(jwk))) {
+    res.statusCode = 400;
+    res.send({
+      status: "error",
+      message: "no tip"
+    });
+  }
+
+  // Return an authentication uri
+  const uri: string = oauthClient.generateAuthUrl({
+    scope: ["openid", "email", "profile"],
+    state: JSON.stringify({
+      address,
+      returnUri: returnURI
+    })
+  });
+
+  res.send({
+    status: "success",
+    uri
+  });
+}
